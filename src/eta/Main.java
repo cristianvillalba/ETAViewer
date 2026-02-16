c2c9ed2104103369a6d5af0f2775106832ee4e89
Cristian Villalba <cristian.villalba@gmail.com>
2026-02-16 17:21:52 -0300

    torsion study
    add a torsion study here

--- src/eta/Main.java
+++ src/eta/Main.java
@@ -1,7 +1,9 @@
 package eta;
 
+import com.jme3.app.LegacyApplication;
 import com.jme3.app.SimpleApplication;
 import com.jme3.collision.CollisionResults;
+import com.jme3.font.BitmapText;
 import com.jme3.input.KeyInput;
 import com.jme3.input.MouseInput;
 import com.jme3.input.controls.ActionListener;
@@ -22,6 +24,7 @@ import com.jme3.scene.Node;
 import com.jme3.scene.shape.Box;
 import com.jme3.scene.shape.Line;
 import com.jme3.scene.shape.Sphere;
+import com.jme3.system.AppSettings;
 import com.simsilica.lemur.Button;
 import com.simsilica.lemur.Command;
 import com.simsilica.lemur.Container;
@@ -76,15 +79,19 @@ Difference between angles
  * 
  */
 public class Main extends SimpleApplication implements ActionListener, AnalogListener{
-    private float realvalue = 1.0f;
-    private float imvalue = 1.0f;
+    private float realvalue = 0.5f;
+    private float imvalue = 0.0f;
     private float imoffset = 0f;
     private Geometry point;
     private Node etanode;
+    private Node overlayetanode;
+    private Node torsionNode;
     private float time = 0f;
     private float steptime = 0f;
     private int stepcounter = 1;
     private int maxsteps = 150;
+    private int globalTorsionIndex = 0;
+    float torsionLenght = 0.0f;
     private Container myMainWindow;
     private Container myStepWindow;
     private TextField realField;
@@ -94,10 +101,13 @@ public class Main extends SimpleApplication implements ActionListener, AnalogLis
     private TextField naturalField;
     private TextField exponentialField;
     private TextField resultComplexField;
+    private BitmapText hudTextInfo;
     private ListBox valueList;
     private float scale = 100f;
     private boolean isdragging = false;
     private ArrayList<Vector3f> temppoints = new ArrayList<Vector3f>();
+    private ArrayList<Float> tempangles = new ArrayList<Float>();
+    private ArrayList<Integer> tempanglescounter = new ArrayList<Integer>();
     
     private boolean GUI = true;
     private boolean automove = false;
@@ -109,21 +119,37 @@ public class Main extends SimpleApplication implements ActionListener, AnalogLis
     private int indexangle = 1;
     
     private int nodeindex = 0;//index to move each index
+    private int iterationanalyze = 0;
     
     public static Main instance;
     
     public static void main(String[] args) {
         Main app = new Main();
+        AppSettings settings = new AppSettings(true);
+        settings.setResolution(1024,768);
+        
+        app.setSettings(settings);
         app.start();
     }
 
-    @Override
+    //@Override
     public void simpleInitApp() {
         instance = this;
         etanode = new Node();
         etanode.setLocalTranslation(0f, 0f, -50f);
         
         rootNode.attachChild(etanode);
+        
+        overlayetanode = new Node();
+        overlayetanode.setLocalTranslation(0f, 0f, -50f);
+        
+        rootNode.attachChild(overlayetanode);
+        
+        torsionNode = new Node();
+        torsionNode.setLocalTranslation(0f, 0f, -50f);
+        
+        rootNode.attachChild(torsionNode);
+        
         
         this.flyCam.setMoveSpeed(100f);
         this.cam.setLocation(new Vector3f(50f, 25f, 500f));
@@ -143,8 +169,19 @@ public class Main extends SimpleApplication implements ActionListener, AnalogLis
 
         InitPoint();
         
+        InitAngles();
+        
         if (GUI){
             InitGUI();
+        }
+    }
+    
+    private void InitAngles()
+    {
+        for (int i = 0 ; i < maxsteps; i++)
+        {
+            tempangles.add(0.0f);
+            tempanglescounter.add(0);
         }
     }
     
@@ -163,7 +200,8 @@ public class Main extends SimpleApplication implements ActionListener, AnalogLis
         inputManager.addMapping("arrowleft", new KeyTrigger(KeyInput.KEY_H));
         inputManager.addMapping("arrowright", new KeyTrigger(KeyInput.KEY_D));
         
-
+        inputManager.addMapping("writeangles", new KeyTrigger(KeyInput.KEY_0));
+        
         inputManager.addListener(this, "Drag");
         inputManager.addListener(this, "Right");
         inputManager.addListener(this, "Left");
@@ -176,6 +214,8 @@ public class Main extends SimpleApplication implements ActionListener, AnalogLis
         inputManager.addListener(this, "arrowdown");
         inputManager.addListener(this, "arrowleft");
         inputManager.addListener(this, "arrowright");
+        
+        inputManager.addListener(this, "writeangles");
     }
     
     private void InitGUI(){
@@ -231,6 +271,8 @@ public class Main extends SimpleApplication implements ActionListener, AnalogLis
                     else
                     {
                         automove = true;
+                        globalTorsionIndex = 0;
+                        torsionLenght = 0;
                     }
                 }
             });
@@ -253,7 +295,13 @@ public class Main extends SimpleApplication implements ActionListener, AnalogLis
         myStepWindow.addChild(exponentialField);
         myStepWindow.addChild(naturalField);
         myStepWindow.addChild(resultComplexField);
-      
+        
+        hudTextInfo = new BitmapText(guiFont, false);
+        hudTextInfo.setSize(guiFont.getCharSet().getRenderedSize());
+        hudTextInfo.setColor(ColorRGBA.White);
+        hudTextInfo.setLocalTranslation(200, hudTextInfo.getLineHeight(), 0);
+        hudTextInfo.setText("angle: ");
+        guiNode.attachChild(hudTextInfo); 
     }
     
     public void CalcETA(boolean focus, int limit, boolean writetofile)
@@ -262,10 +310,10 @@ public class Main extends SimpleApplication implements ActionListener, AnalogLis
             if (GUI){
                 realvalue = Float.parseFloat(realField.getText());
                 imvalue = Float.parseFloat(imgField.getText());
-                point.setLocalTranslation(realvalue * scale, (imvalue - imoffset)*scale, 0f);
+                //point.setLocalTranslation(realvalue * scale, (imvalue - imoffset)*scale, 0f);
             }
             
-            ConstructETA(limit, writetofile);
+            ConstructETA(limit, writetofile, true);
             
             if (GUI && focus){
                 GuiGlobals.getInstance().releaseFocus(myMainWindow);
@@ -353,24 +401,30 @@ public class Main extends SimpleApplication implements ActionListener, AnalogLis
             FileWriter myWriter = new FileWriter(name, true);
             myWriter.write(data);
             myWriter.close();
-            System.out.println("Successfully wrote to the file.");
+            //System.out.println("Successfully wrote to the file.");
           } catch (IOException e) {
             System.out.println("An error occurred.");
             e.printStackTrace();
           }
     }
     
-    private void ConstructETA(int limit, boolean writetofile)
+    private void ConstructETA(int limit, boolean writetofile, boolean writeangles)
     {
         temppoints.clear();
         temppoints.add(new Vector3f());
         
         etanode.detachAllChildren();
+        torsionNode.detachAllChildren();
         
         Complex etasum = new Complex(0f, 0f);
         Complex one = new Complex(1f, 0f);
         Complex expon = new Complex(realvalue, imvalue);
         Complex segmentdata = new Complex(0f, 0f);
+        Complex segmentdataprev = null;
+        Complex analyzeorigin = null;
+        Complex etasumprev = null;
+        Complex anglecalculationV1 = null;
+        boolean writeanalyze = false;
         
         etanode.attachChild(GeneratePoint(0f, 0f, 0));
         
@@ -387,11 +441,21 @@ public class Main extends SimpleApplication implements ActionListener, AnalogLis
             this.CreateFile(randfilename);
         }
         
+        //------------
+        float torsionIndex = 3.1214256838f * globalTorsionIndex + 7.8518771786f;
+        float torsionIndexNext = 3.1214256838f * (globalTorsionIndex + 1) + 7.8518771786f;
+      
+        boolean torsionDetected = false;
+        hudTextInfo.setText("torsionIndex: " + torsionIndex + " torsionNext: " + torsionIndexNext);
+        //------------
+        
         for (int i = 1; i < limit; i++)
         {
             Complex divisor = new Complex(i, 0f);
             
             segmentdata = one.divide(divisor.pow(expon));//save segment data to visualize
+            
+            etasumprev = etasum; //store previous eta value
             
             if ( i % 2 == 1){
                 etasum = etasum.add(one.divide(divisor.pow(expon)));
@@ -402,21 +466,104 @@ public class Main extends SimpleApplication implements ActionListener, AnalogLis
                 segmentdata = segmentdata.multiply(-1.0f);//correct sign of individual term
             }
             
+            if (imvalue > torsionIndex && imvalue < torsionIndexNext  && (globalTorsionIndex + 3) == i )
+            {
+                float segmentTorsionScale = (imvalue - torsionIndex )/ (torsionIndexNext - torsionIndex);
+                Complex segmentTorsion = etasumprev.add(segmentdata.multiply(segmentTorsionScale));
+                
+                ConstructTorsion((float)segmentTorsion.getReal(), (float)segmentTorsion.getImaginary());
+                
+                if (Math.abs(imvalue - torsionIndexNext) < 0.1 && !torsionDetected)
+                {
+                    globalTorsionIndex = globalTorsionIndex + 1;
+                    torsionDetected = true; //prevent index to blow
+                }
+             
+            }
+            
             if (writetofile){
                 this.WriteToFile(randfilename, i + ", " + segmentdata.getReal() + ", " + segmentdata.getImaginary() + ", " + etasum.getReal() + ", " + etasum.getImaginary() + ", " + (segmentdata.getArgument() + Math.PI*2)+ ", " + Math.toDegrees((segmentdata.getArgument() + Math.PI*2)) + "\n");
+            }
+            
+            if (segmentdataprev != null)
+            {
+                if (angleIsZero(segmentdataprev.getReal(), segmentdataprev.getImaginary(), 
+                                segmentdata.getReal(), segmentdata.getImaginary()))
+                {
+                    System.out.println("Angle 0 in iteration: " + i);
+                    System.out.println("Im number: " + imvalue);
+                    
+                    if (i > iterationanalyze && !writeanalyze)
+                    {
+                        iterationanalyze = i;
+                        writeanalyze = true;
+                        analyzeorigin = etasumprev;
+                        anglecalculationV1 = segmentdata.subtract(segmentdataprev);
+                    }
+                }
             }
             
             etanode.attachChild(GeneratePoint( (float)etasum.getReal()*scale, (float)etasum.getImaginary()*scale,i));
             etanode.attachChild(GenerateLine(previousreal, previousimg,(float)etasum.getReal(), (float)etasum.getImaginary(),i ));
             
+            //calculate how many full turns
+            if (writeangles)
+            {
+                float angle = (float) segmentdata.getArgument();
+                
+                if ((i % 2) == 0)
+                {
+                    angle = (angle - FastMath.PI) * -1.0f; 
+                }
+                else
+                {
+                    if (angle < 0)
+                        angle *= -1.0f;
+                    else
+                        angle = FastMath.PI + (FastMath.PI - angle);
+                }
+
+                if (FastMath.abs(angle - tempangles.get(i)) > (FastMath.TWO_PI * 0.95f)) //detects a complete circle rotation
+                {
+                    tempanglescounter.set(i, tempanglescounter.get(i) + 1);
+                    //System.out.println("Count 360!");
+                }
+                
+                if (i == 4) //it starts with 2 as the first segment
+                {
+                    //hudTextInfo.setText("angle: " + angle);
+                }
+                tempangles.set(i, angle);  
+            }
+            //calculate how many full turns
+            
             previousreal =  (float)etasum.getReal();
             previousimg = (float)etasum.getImaginary();
+            
+            
+            segmentdataprev = segmentdata;
             
             temppoints.add(new Vector3f(previousreal, previousimg, 0.0f));
         }
         
         //etanode.attachChild(GenerateLine(realvalue, imvalue - imoffset, previousreal, previousimg, 666)); //remove red line
         resultComplexField.setText("Result: " + df.format(segmentdata.getReal()) + " " + df.format(segmentdata.getImaginary()) + "i");
+        
+        if (writeangles && FastMath.abs((float)etasum.getReal()) < 0.1f && FastMath.abs((float)etasum.getImaginary()) < 0.1f)
+        {
+            WriteAngles(randfilename);
+        }
+        
+        if (writeanalyze)
+        {
+            Complex anglecalculationV2 = etasum.subtract(analyzeorigin);
+            float angle = angleBetweenComplex((float)anglecalculationV1.getReal(), (float)anglecalculationV1.getImaginary(),(float) anglecalculationV2.getReal(), (float)anglecalculationV2.getImaginary());
+            
+            System.out.println("iteration: " + iterationanalyze + " real: " + df.format(etasum.getReal() - analyzeorigin.getReal()) + " img: " + df.format(etasum.getImaginary()- analyzeorigin.getImaginary()));
+            System.out.println("Angle - - - : " + angle);
+            
+            overlayetanode.attachChild(GenerateLine((float)analyzeorigin.getReal(), (float)analyzeorigin.getImaginary(),(float)etasum.getReal(), (float)etasum.getImaginary(),666 ));
+        }
     }
     
     private void ConstructETAToAnalyze(int limit, float name)
@@ -515,12 +662,16 @@ public class Main extends SimpleApplication implements ActionListener, AnalogLis
         Geometry linexpos = new Geometry("etaline" + index, linexp);
         Material mat1 = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
         
-        if (index != 666){
-            mat1.setColor("Color", ColorRGBA.Green);
+        if (index == 666){
+            mat1.setColor("Color", ColorRGBA.Red);
+        }
+        else if (index == 667)
+        {
+            mat1.setColor("Color", ColorRGBA.Orange);
         }
         else
         {
-            mat1.setColor("Color", ColorRGBA.Red);
+            mat1.setColor("Color", ColorRGBA.Green);
         }
         linexpos.setMaterial(mat1);
         
@@ -829,7 +980,10 @@ public class Main extends SimpleApplication implements ActionListener, AnalogLis
             nodeindex++;
         }
         
-        
+        if (name.equals("writeangles") && isPressed)
+        {
+            WriteAngles("nafta.csv");
+        }
 
     }
 
@@ -863,5 +1017,82 @@ public class Main extends SimpleApplication implements ActionListener, AnalogLis
         else{
             etanode.rotate(0, 0, -0.01f);
         }
+    }
+    
+    private void WriteAngles(String filename)
+    {
+         this.WriteToFile(filename, "i: " + imvalue + "\n");
+         
+         StringBuilder sbangles = new StringBuilder();
+         StringBuilder sbcounter = new StringBuilder();
+         
+         for (int i = 0; i < tempangles.size(); i++)
+         {
+             sbangles.append(tempangles.get(i).toString() + ",");
+             Integer counter = new Integer(tempanglescounter.get(i) - 1);
+             
+             sbcounter.append(counter.toString() + ",");
+         }
+         
+         sbangles.append("\n");
+         sbcounter.append("\n");
+         
+         this.WriteToFile(filename, sbangles.toString());
+         this.WriteToFile(filename, sbcounter.toString());
+    }
+    
+    public static boolean angleIsZero(
+        double prevDx, double prevDy,
+        double currDx, double currDy) 
+    {
+
+        // producto cruzado en 2D
+        double cross = prevDx * currDy - prevDy * currDx;
+
+        // si el cross no es ≈ 0 → NO son colineales → no puede ser 0°
+        if (Math.abs(cross) > 0.001)
+            return false;
+
+        // producto punto
+        double dot = prevDx * currDx + prevDy * currDy;
+
+        // si el dot > 0 → apuntan al MISMO lado → ángulo 0°
+        return dot > 0;
+    }
+    
+    public static float angleBetweenComplex(
+        float r1, float i1,
+        float r2, float i2)
+    {
+        float dot = r1 * r2 + i1 * i2;
+        float mag1 = FastMath.sqrt(r1 * r1 + i1 * i1);
+        float mag2 = FastMath.sqrt(r2 * r2 + i2 * i2);
+
+        if (mag1 == 0 || mag2 == 0) {
+            return 0f; // vector degenerado
+        }
+
+        float cos = dot / (mag1 * mag2);
+
+        // seguridad numérica
+        cos = FastMath.clamp(cos, -1f, 1f);
+
+        return FastMath.acos(cos); // en radianes
+    }
+    
+    private void ConstructTorsion(float r1param, float i1param)
+    {
+        float r1 = r1param;
+        float i1 = i1param;
+        
+        //float r2 = r1 + 0.72f * (float)Math.cos(5.0f - 0.05f * (imvalue));
+        //float i2 = i1 + 0.72f * (float)Math.sin(5.0f - 0.05f * (imvalue));
+        
+        float exponent = FastMath.pow(imvalue, 1.05f);
+        float r2 = r1 + 0.72f * (float)Math.cos(5.0f - 1.39f * (exponent));
+        float i2 = i1 + 0.72f * (float)Math.sin(5.0f - 1.39f * (exponent));
+        
+        
+        torsionNode.attachChild(GenerateLine(r1,i1,r2,i2,667));
     }
 }
